e18ae9df3b309bc351e393689ed8fadcf62fab2c
phipsi99 <schoppphilipp@googlemail.com>
2024-12-20 19:48:13 +0100

    added 1-6

--- 03/main.py
+++ 03/main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 
 def do_main(debug_mode=False):
@@ -11,7 +12,49 @@ def do_main(debug_mode=False):
     point_sum = 0
 
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        visited = Counter([(0,0)])
+        pos = [0,0]
+        for c in line:
+            if c == ">":
+                pos[0] += 1
+            elif c == "<":
+                pos[0] -= 1
+            elif c == "^":
+                pos[1] -= 1
+            elif c == "v":
+                pos[1] += 1
+            visited[(pos[0], pos[1])] += 1
+        print(len(visited))
+
+        visited = Counter([(0,0)])
+        pos = [0,0]
+        pos2 = [0,0]
+        turn1 = True
+        for c in line:
+            if turn1:
+                if c == ">":
+                    pos[0] += 1
+                elif c == "<":
+                    pos[0] -= 1
+                elif c == "^":
+                    pos[1] -= 1
+                elif c == "v":
+                    pos[1] += 1
+                visited[(pos[0], pos[1])] += 1
+                turn1 = False
+            else:
+                if c == ">":
+                    pos2[0] += 1
+                elif c == "<":
+                    pos2[0] -= 1
+                elif c == "^":
+                    pos2[1] -= 1
+                elif c == "v":
+                    pos2[1] += 1
+                visited[(pos2[0], pos2[1])] += 1
+                turn1 = True
+        print(len(visited))
+
 
 if __name__ == '__main__':
     do_main(False)
--- 06/main.py
+++ 06/main.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import re
+
+import numpy as np
 
 def do_main(debug_mode=False):
     with open(Path('06/input.txt')) as file:
@@ -10,8 +13,45 @@ def do_main(debug_mode=False):
 
     point_sum = 0
 
+    lights = np.full((1000, 1000), 0, dtype=int)
+
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        nums = re.findall(r"(\d+),(\d+)",line)
+        x = int(nums[0][0])
+        y = int(nums[0][1])
+        xx = int(nums[1][0])
+        yy = int(nums[1][1])
+        if "on" in line:
+            lights[x:xx+1, y:yy+1] = 1
+        elif "off" in line:
+            lights[x:xx+1, y:yy+1] = 0
+        elif "toggle" in line:
+            lights[x:xx+1, y:yy+1] = lights[x:xx+1, y:yy+1] ^ 1
+    print(np.count_nonzero(lights == 1))
+    lights = np.full((1000, 1000), 0, dtype=int)
+
+
+    for line_index, line in enumerate(lines):
+        nums = re.findall(r"(\d+),(\d+)",line)
+        x = int(nums[0][0])
+        y = int(nums[0][1])
+        xx = int(nums[1][0])
+        yy = int(nums[1][1])
+        if "on" in line:
+            lights[x:xx+1, y:yy+1] += 1
+        elif "off" in line:
+            lights[x:xx+1, y:yy+1] -=1
+            smaller = np.where(lights < 0)
+            for i, j in zip(smaller[0], smaller[1]):
+                lights[i,j] = 0
+        elif "toggle" in line:
+            lights[x:xx+1, y:yy+1] += 2
+    
+    for i in range(1000):
+        for j in range(1000):
+            point_sum += lights[i, j]
+    print(point_sum)
+
 
 if __name__ == '__main__':
     do_main(False)
